e5b4e94f27a4331d249e97f30704e8a46dfaabc4
dingpengqiang <dpq15120593518>
2019-12-20 17:43:13 +0800

    定时器循环发送消息

--- app/src/main/java/com/moway/okhttp3_websocket/MainActivity.java
+++ app/src/main/java/com/moway/okhttp3_websocket/MainActivity.java
@@ -2,13 +2,99 @@ package com.moway.okhttp3_websocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start);
+        text = (TextView) findViewById(R.id.text);
+
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                connect();
+            }
+        });
+    }
+    private void connect() {
+
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        Request request = new Request.Builder()
+                .url("ws://echo.websocket.org")
+                .build();
+        OkHttpClient client = new OkHttpClient();
+        client.newWebSocket(request, listener);
+
+        client.dispatcher().executorService().shutdown();
+    }
+
+
+    private void output(final String content) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(text.getText().toString() + content + "\n");
+            }
+        });
+
+    }
+    private final class EchoWebSocketListener extends WebSocketListener {
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+
+            webSocket.send("hello world");
+            webSocket.send("welcome");
+            webSocket.send(ByteString.decodeHex("adef"));
+            webSocket.close(1000, "再见");
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            output("onMessage: " + text);
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            output("onMessage byteString: " + bytes);
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            webSocket.close(1000, null);
+            output("onClosing: " + code + "/" + reason);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket, int code, String reason) {
+            output("onClosed: " + code + "/" + reason);
+            startActivity(new Intent(MainActivity.this,SecondActivity.class));
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            output("onFailure: " + t.getMessage());
+        }
     }
 }
--- app/src/main/java/com/moway/okhttp3_websocket/SecondActivity.java
+++ app/src/main/java/com/moway/okhttp3_websocket/SecondActivity.java
@@ -3,12 +3,126 @@ package com.moway.okhttp3_websocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnStart;
+    private TextView tvOutput;
+    private WebSocket mSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        btnStart = (Button) findViewById(R.id.start);
+        tvOutput = (TextView) findViewById(R.id.output);
+        btnStart.setOnClickListener(this);
+
     }
+
+    private void start() {
+        OkHttpClient mOkHttpClient = new OkHttpClient.Builder()
+                .readTimeout(3, TimeUnit.SECONDS)//设置读取超时时间
+                .writeTimeout(3, TimeUnit.SECONDS)//设置写的超时时间
+                .connectTimeout(3, TimeUnit.SECONDS)//设置连接超时时间
+                .build();
+
+        Request request = new Request.Builder().url("ws://echo.websocket.org").build();
+        EchoWebSocketListener socketListener = new EchoWebSocketListener();
+        mOkHttpClient.newWebSocket(request, socketListener);
+        mOkHttpClient.dispatcher().executorService().shutdown();
+    }
+
+    /**
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start://开始连接
+                start();
+                break;
+        }
+    }
+
+    private final class EchoWebSocketListener extends WebSocketListener {
+
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            super.onOpen(webSocket, response);
+            mSocket = webSocket;
+            String openid = "1";
+            //连接成功后，发送登录信息
+            String message = "{\"type\":\"login\",\"user_id\":\""+openid+"\"}";
+            mSocket.send(message);
+            output("连接成功！");
+
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            super.onMessage(webSocket, bytes);
+            output("receive bytes:" + bytes.hex());
+        }
+
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+            super.onMessage(webSocket, text);
+            output("receive text:" + text);
+            //收到服务器端发送来的信息后，每隔2秒发送一次心跳包
+            final String message = "{\"type\":\"heartbeat\",\"user_id\":\"heartbeat\"}";
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mSocket.send(message);
+                }
+            },2000);
+        }
+
+        @Override
+        public void onClosed(WebSocket webSocket, int code, String reason) {
+            super.onClosed(webSocket, code, reason);
+            output("closed:" + reason);
+        }
+
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            super.onClosing(webSocket, code, reason);
+            output("closing:" + reason);
+        }
+
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            super.onFailure(webSocket, t, response);
+            output("failure:" + t.getMessage());
+        }
+    }
+
+    private void output(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tvOutput.setText(tvOutput.getText().toString() + "\n\n" + text);
+            }
+        });
+    }
+
 }
+

89e21a685d5fbb378dcd30ac71a364e49244e485
dingpengqiang <dpq15120593518>
2019-12-20 17:36:35 +0800

    首次提交，初始换项目

---
+++ app/src/main/java/com/moway/okhttp3_websocket/MainActivity.java
@@ -0,0 +1,14 @@
+package com.moway.okhttp3_websocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
